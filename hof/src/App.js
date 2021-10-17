1efb92210f56e33e17c8daca0fb0df2a874cc537
Tejas1004 <tejaszanzane10@gmail.com>
2021-10-17 13:50:41 +0530

    master of hof done

---
+++ hof/src/App.js
@@ -0,0 +1,12 @@
+import HigherOrderFunctions from "./components/HigherOrderComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderFunctions />
+    </div>
+  );
+}
+
+export default App;
